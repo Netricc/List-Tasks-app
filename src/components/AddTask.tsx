a116a142600833eea439ae14075a3941712fac71
Nawar <nawabsn911@gmail.com>
2025-01-24 21:38:15 +0100

    adding github btn + notask logic

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -31,19 +31,27 @@ const AddTask = ({
   };
 
   return (
-    <section className="w-[30%] bg-slate-200 flex flex-col items-center pt-[24px] h-full">
-      <input
-        className="w-[360px] h-[45px] bg-blue-400 py-1.5 px-[16px] rounded-lg border-2 border-black text-white text-lg outline-0"
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Add Task"
-      />
-      <button
-        onClick={handleAddTask}
-        className="mt-[24px] bg-blue-400 py-[9px] px-[16px] rounded-full cursor-pointer hover:bg-blue-500 transition-colors"
-      >
-        Add Task
+    <section className="w-[30%] bg-slate-200 flex items-center flex-col gap-[300px] py-[24px] h-full">
+      <div className="flex flex-col gap-[16px]">
+        <input
+          className="w-[360px] h-[45px] bg-blue-400 py-1.5 px-[16px] rounded-lg border-2 border-black text-white text-lg outline-0"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Add Task"
+        />
+        <button
+          onClick={handleAddTask}
+          className="mt-[24px] bg-blue-400 py-[9px] px-[16px] rounded-full cursor-pointer hover:bg-blue-500 transition-colors"
+        >
+          Add Task
+        </button>
+      </div>
+
+      <button className="py-[9px] px-[12px] bg-black text-white rounded-[3px] hover:bg-slate-800 transition-colors">
+        <a href="https://github.com/Netricc/List-Tasks-app/tree/main">
+          Github Rpository
+        </a>
       </button>
     </section>
   );
--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -33,34 +33,47 @@ const TasksList = ({
   };
   return (
     <section className="w-[70%] h-full bg-[#ececec] container-max py-[40px] overflow-auto">
-      <div className="w-full flex flex-col gap-[20px]">
-        {tasks.map(
-          (task: { title: string; id: number; completed: boolean }) => (
-            <div
-              key={task.id}
-              className="flex w-[60%] bg-slate-50 h-[60px] items-center  justify-between  px-[14px] py-[10px] "
-            >
-              <h3
-                onClick={() => handleCompleteTask(task.id)}
-                className={`${
-                  task.completed
-                    ? "line-through text-slate-400"
-                    : "no-underline text-black"
-                } cursor-pointer text-xl font-medium select-none`}
+      {tasks.length > 0 ? (
+        <div className="w-full flex flex-col gap-[20px]">
+          {tasks.map(
+            (task: { title: string; id: number; completed: boolean }) => (
+              <div
+                key={task.id}
+                className="flex w-[60%] bg-slate-50 h-[60px] items-center  justify-between  px-[14px] py-[10px] "
               >
-                {task.id} - {task.title}
-              </h3>
+                <h3
+                  onClick={() => handleCompleteTask(task.id)}
+                  className={`${
+                    task.completed
+                      ? "line-through text-slate-400"
+                      : "no-underline text-black"
+                  } cursor-pointer text-xl font-medium select-none`}
+                >
+                  {task.id} - {task.title}
+                </h3>
 
-              <button
-                onClick={() => handleDeleteTask(task.id)}
-                className="cursor-pointer hover:bg-red-500 p-[6px] rounded-full transition-colors select-none"
-              >
-                <img src={trashIcon} width={20} height={20} alt="trash icon" />
-              </button>
-            </div>
-          )
-        )}
-      </div>
+                <button
+                  onClick={() => handleDeleteTask(task.id)}
+                  className="cursor-pointer hover:bg-red-500 p-[6px] rounded-full transition-colors select-none"
+                >
+                  <img
+                    src={trashIcon}
+                    width={20}
+                    height={20}
+                    alt="trash icon"
+                  />
+                </button>
+              </div>
+            )
+          )}
+        </div>
+      ) : (
+        <div>
+          <p className="text-lg font-semibold text-slate-500">
+            there is no tasks in the list
+          </p>
+        </div>
+      )}
     </section>
   );
 };
